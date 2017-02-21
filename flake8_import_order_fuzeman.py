1c22f2d589fee42254f0432a22be10812b8ceb3a
Dean Gardiner <me@dgardiner.net>
2017-02-22 10:21:22 +1300

    `collections`, `datetime` and `decimal` are now `from ...` importable

--- flake8_import_order_fuzeman.py
+++ flake8_import_order_fuzeman.py
@@ -12,7 +12,12 @@ __all__ = 'Fuzeman',
 
 
 class Fuzeman(Style):
-    from_importable_standard_librarires = frozenset(['typing'])
+    from_importable_standard_librarires = frozenset([
+        'collections',
+        'datetime',
+        'decimal',
+        'typing'
+    ])
 
     @staticmethod
     def name_key(identifier):
